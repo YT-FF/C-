6477ee55085e35ba1121ead76449ecb0fec97821
xiayijun <yangtuoxyj@outlook.com>
2020-10-14 10:58:41 +0800

    添加项目文件。

---
+++ ClassLibrary1/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class User
+    {
+        private int a, b, x;
+        public User(int a,int b,int x)
+        {
+            a = a;
+            b = b;
+            x = x;
+            if ((a > 1) && (b == 0))
+            {
+                x = x / a;
+            }
+            if ((a == 2) || (x > 1))
+            {
+                x += 1;
+            }
+        }
+        
+    }
+}
---
+++ ClassLibrary1Tests/UserTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.Tests
+{
+    [TestClass()]
+    public class UserTests
+    {
+        [TestMethod()]
+        public void UserTest()
+        {
+            int a=1, b=0, x=0;
+            User target = new User(a, b, x);
+            Assert.IsTrue(target != null);
+        }
+
+        [TestMethod()]
+        public void UserTest1()
+        {
+            int a = -1, b = 0, x = 2;
+            User target = new User(a, b, x);
+            Assert.IsTrue(target != null);
+        }
+
+        [TestMethod()]
+        public void UserTest2()
+        {
+            int a =2, b = 1, x = 2;
+            User target = new User(a, b, x);
+            Assert.IsTrue(target != null);
+        }
+        [TestMethod()]
+        public void UserTest3()
+        {
+            int a = 2, b = 0, x = 4;
+            User target = new User(a, b, x);
+            Assert.IsTrue(target != null);
+        }
+
+    }
+}
